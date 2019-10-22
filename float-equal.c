d79fb163f746c8d2c9bdcf36570577444f14ed01
guoronggeng <56394288+guoronggeng@users.noreply.github.com>
2019-10-22 08:16:00 +0800

    Add files via upload

--- float-equal.c
+++ float-equal.c
@@ -1,13 +1,16 @@
-//实现代码判断2个float变量相等
+//
 //误差精确到时小数点后5位
 #include <stdio.h>
+#include <math.h>
+
 int main()
-{
+{  
     float m = 1.78;
     float x = 1.779999, y = 2;
     float z = m / y, z1 = x / y;
-    if (z == z1) {
+    if (fabs(z1 - z)<1e-5) {
         z = (m + x) / y;
+    
     }
     printf("Z is %f\n", z);
     printf("Z1 is %f\n", z1);
